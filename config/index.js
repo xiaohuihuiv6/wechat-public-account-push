ed19030dd0f3cdbc472bb8c22ae91d884a0d834f
xiaohuihuiv6 <112753470+xiaohuihuiv6@users.noreply.github.com>
2022-09-04 10:37:36 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,23 +1,2 @@
 export const config = {
-# 公众号配置
-# 公众号appId
-"app_id": "wx3f444c2245e310ee",
-# 公众号appSecret
-"app_secret": "e4adf28b3b86b2ba729a0c048a507ccb",
-# 模板消息id
-"template_id": "cmTDA-NW-EHt_vnV4J0V9saThVyjchGSJuXdMSz_GzU",
-# 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-"user": ["ozQjX5iUKi5QGgYg1IESY71Z2TqE"],
-
-# 信息配置
-# 所在省份
-"province": "陕西",
-# 所在城市
-"city": "渭南",
-# 生日1，修改名字为对应需要显示的名字，如果生日为农历，在最前面加上r即可
-"birthday1": {"name": "小宝", "birthday": "r1997-07-20"},
-# 生日2
-"birthday2": {"name": "小林", "birthday": "1997-08-18"},
-# 在一起的日子，格式同上
-"love_date": "2022-01-01"
-}
+  
